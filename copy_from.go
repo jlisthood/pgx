0f0c5f49b244a1d190bf466346261875abdff7aa
Jesse Liston <jesse.liston@robinhood.com>
2022-06-08 16:42:06 -0700

    wow

--- copy_from.go
+++ copy_from.go
@@ -153,7 +153,7 @@ func (ct *copyFrom) run(ctx context.Context) (int64, error) {
 		}
 
 		fmt.Printf("closing...\n")
-		_, err = w.Write([]byte{8, 8, 8, 8})
+		_, err = w.Write([]byte{8, 8, 8})
 		w.Close()
 		fmt.Printf("closing done...\n")
 	}()
